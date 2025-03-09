fc2e1fe40631efae07c0399d426928c0fa2fe207
Callie Holmes <github@kimiscool.me>
2025-03-09 16:41:15 -0700

    Finish LinkedSubmenuComponent problem

--- src/app/problems/LinkedSubmenuComponent.tsx
+++ src/app/problems/LinkedSubmenuComponent.tsx
@@ -1,14 +1,29 @@
 import * as React from "react";
 import * as problemsData from '../content/problems.json';
 import { FileNode } from "../structures/fileTree";
+import { nodeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin";
 
 export default function LinkedSubmenuComponent() {
     const { problems } = problemsData;
     const { name, description, tags, difficulty, providedContent } = problems[2];
+    let root = providedContent[0].nodes[0];
 
-    const flattenTree = (node: FileNode) => {
-
+    const flattenTree = ( node: FileNode, key: number = 0) => {
+        if (!node) return null;
+        return (
+            <li key={key}>
+                {node.name}
+                {node.isDirectory && node.children && (
+                    <ul>
+                        {node.children.map((child, idx) => flattenTree(child, key * 10 + idx))}
+                    </ul>
+                )}
+                </li>
+        )
     }
+
+    const createdList = flattenTree(root);
+    console.log('theList', createdList);
 
     return(
         <div>
@@ -20,7 +35,7 @@ export default function LinkedSubmenuComponent() {
             <div className="description">{description}</div>
 
             <div className="linked-submenu">
-                
+                {createdList}
             </div>
         </div>
     )
